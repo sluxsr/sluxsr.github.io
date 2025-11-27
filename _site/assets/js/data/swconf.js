093c297b168150061124a44a0fddcb79cfec7a71
Shengyao Lu <43564531+sluxsr@users.noreply.github.com>
2025-11-27 01:46:08 -0800

    。

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1764236087',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/about/',
+      
+        '/publications/',
+      
+        '/teaching/',
+      
+        '/service/',
+      
+        '/misc/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
