dfc6453db54a27b5a344e637322ca71da72de817
Shengyao Lu <43564531+sluxsr@users.noreply.github.com>
2025-11-27 02:11:20 -0800

    .

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764236087',resources: [
+    cacheName: 'chirpy-1764238072',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
